c7d856fd5438dbf8acd870c9a00cba0e1422060c
Pavlos Simas <simaspavlos@gmail.com>
2023-10-21 21:43:55 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Resolver",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v15)
+    ],
     products: [
         .library(
             name: "Resolver",
